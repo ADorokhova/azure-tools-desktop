054acbfd6947cd017d1b3949ed00d4f8aae221b9
dorokhov.vladimir@gmail.com <dorokhov.vladimir@gmail.com>
2016-02-16 20:36:50 +0200

    setting commands

--- app/common/services/appSettings.js
+++ app/common/services/appSettings.js
@@ -13,30 +13,34 @@
         require('electron').remote.getCurrentWindow().reload();
     };
 
-    try {
-        self.current = new ElectronSettings({
-            configDirPath: path
-        });
-    } catch (e) {
-        dialog.showMessageBox({
-            type: 'error',
-            title: 'Azure Tools',
-            buttons: ['Yes, I want clean up config and work safely', 'No, I will fix settings file by myself'],
-            message: String.format('Error occured trying to read application settings from "{0}". {1}Do you want to clean up settings file to continue your work safely?\n\nDetails: {2}', application.getPath('userData'), '\n\n', e.message),
-        },
-        function (x) {
-            if (x === 0) {
-                fileService.writeToFile(require('path').join(application.getPath('userData'), 'settings.json'), '{}', function success() {
-                    dialog.showMessageBox({
-                        type: 'info',
-                        title: 'Azure Tools',
-                        buttons: ['Ok'],
-                        message: 'Settings cleaned up successsfully'
-                    });
+    self.init = function() {
+        try {
+            self.current = new ElectronSettings({ configDirPath : path });
+        } catch (e) {
+            dialog.showMessageBox({
+                    type: 'error',
+                    title: 'Azure Tools',
+                    buttons: ['No, I will fix settings file by myself', 'Yes, I want clean up config and work safely'],
+                    message: String.format('Error occured trying to read application settings from "{0}". {1}Do you want to clean up settings file to continue your work safely?\n\nDetails: {2}', application.getPath('userData'), '\n\n', e.message),
+                },
+                function (x) {
+                    console.log('-- dialog choice--');
+                    console.log(x);
+                    if (x === 1) {
+                        fileService.writeToFile(require('path').join(application.getPath('userData'), 'settings.json'), '{}', function success() {
+                            dialog.showMessageBox({
+                                type: 'info',
+                                title: 'Azure Tools',
+                                buttons: ['Ok'],
+                                message: 'Settings cleaned up successsfully'
+                            });
+                        });
+                    }
                 });
-            }
-        });
-    }
+        }
+    };
+
+    self.init();
 
     return self;
 };
--- app/common/services/settingsCommands.js
+++ app/common/services/settingsCommands.js
@@ -1,6 +1,18 @@
 exports.create =
     function ($rootScope, dialog, fileService, appSettings) {
         var self = this;
+
+        function writeToSettingsFile(data, cb) {
+            fileService.writeToFile(appSettings.current.getConfigFilePath(), data, function success() {
+                appSettings.recreate(function success() {
+                    console.log("The file was saved!");
+                    if (cb) {
+                        cb();
+                    }
+                });
+            });
+        };
+
         self.import = function (cb) {
 
             dialog.showOpenDialog({
@@ -11,15 +23,14 @@ exports.create =
                 if (fileNames === undefined) return;
                 var fileName = fileNames[0];
                 fileService.readFromFile(fileName, function success(data) {
-                    console.log(String.format('import data {0} to {1}', data, appSettings.current.getConfigFilePath()));
-                    fileService.writeToFile(appSettings.current.getConfigFilePath(), data, function success() {
-                        appSettings.recreate(function success() {
-                            console.log("The file was saved!");
-                            if (cb) {
-                                cb();
-                            }
-                        });
-                    });
+                    if (!appSettings.current) {
+                        appSettings.init();
+                    }
+
+                    if (appSettings.current) {
+                        console.log(String.format('import data {0} to {1}', data, appSettings.current.getConfigFilePath()));
+                        writeToSettingsFile(data, cb);
+                    }
                 });
             });
         };
